a558997433c288f5f5e52ec56ca5a762900a3a12
young <young@young-X401A>
2017-03-29 07:45:39 +0800

    更新网络监控进程bug

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import getInfo
 import time
 from multiprocessing import Process, Queue
 from operator import or_
+from gevent.hub import sleep
+from networkx.algorithms.components.connected import is_connected
 
-#开两个线程，一个监视网络是否连接，一个用于重启sslocal
+#开两个线程，一个监视网络密码端口是否更改，一个用于重启sslocal
 
 
 def isConnected():#判断获取的ＩＰ是否还有效果
+    '''
     counter=0
     temp=0
     data_dic0,data_dic1=getInfo.getData()
@@ -28,7 +31,16 @@ def isConnected():#判断获取的ＩＰ是否还有效果
                 restart_server()#连续3次ping不通，就重启服务
             counter=0
             temp=0
-
+    '''
+    data_dic0_st,data_dic1_st=getInfo.getData()
+    while True:
+        data_dic0,data_dic1=getInfo.getData()
+        if (data_dic0!=data_dic0_st or data_dic1!=data_dic1_st):
+            print("更新密码、端口中...")
+            restart_server()
+            data_dic0_st,data_dic1_st=getInfo.getData()
+            sleep(1)
+        
 
 def start_server():
         json_path=os.getcwd()
